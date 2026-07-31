29426c047ed3b975925850fac7fb7135d8f947b6
Kushal Kabiraj <suklakabiraj2@gmail.com>
2026-07-31 20:54:31 +0530

    Add solution for Maximum Product of Two Elements in an Array

--- LeetCode/Maximum-Product-of-Two-Elements-in-an-Array/Solution.java
+++ LeetCode/Maximum-Product-of-Two-Elements-in-an-Array/Solution.java
@@ -1,16 +1,8 @@
-1class Solution {
-2    public int maxProduct(int[] nums) {
-3        int max1 = 0, max2 = 0;
-4
-5        for (int num : nums) {
-6            if (num >= max1) {
-7                max2 = max1;
-8                max1 = num;
-9            } else if (num > max2) {
-10                max2 = num;
-11            }
-12        }
-13
-14        return (max1 - 1) * (max2 - 1);
-15    }
-16}
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        curr_max = 1
+        res = 0
+        for i in range(len(nums)):
+            res = max(res, (nums[i] - 1) * (curr_max - 1))
+            curr_max = max(curr_max, nums[i])
+        return res
