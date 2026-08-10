4f2700c8bff02a9eccbc1644a6c116a5867c54b7
Kushal Kabiraj <suklakabiraj2@gmail.com>
2026-08-10 20:06:34 +0530

    Add solution for Stone Game IV

---
+++ LeetCode/Stone-Game-IV/Solution.java
@@ -0,0 +1,16 @@
+1class Solution {
+2    public boolean winnerSquareGame(int n) {
+3        boolean[] dp = new boolean[n + 1];
+4
+5        for (int i = 1; i <= n; i++) {
+6            for (int k = 1; k * k <= i; k++) {
+7                if (!dp[i - k * k]) {
+8                    dp[i] = true;
+9                    break;
+10                }
+11            }
+12        }
+13
+14        return dp[n];
+15    }
+16}
